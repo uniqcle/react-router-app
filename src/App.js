d6cdaa3d5e0b95894fe72425fd59d112c49fee93
uniqcle <uniqcle@yandex.ru>
2021-12-17 16:20:22 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container">
+
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" render={(props) => <Products {...props} sortyBy="newest" />} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/admin/sidebar.jsx
@@ -0,0 +1,22 @@
+import React, { Component, createContext } from 'react';
+import { Link } from 'react-router-dom'
+
+class SideBar extends Component {
+
+    render() {
+        return (
+
+            <ul>
+                <li>
+                    <Link to="/admin/posts">Posts</Link>
+                </li>
+                <li>
+                    <Link to="/admin/users">Users</Link>
+                </li>
+            </ul>
+
+        );
+    }
+}
+
+export default SideBar;
